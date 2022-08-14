9e74ef5729f20347c9fe54d003fc2d5ef11bd77a
Sebastian Enrique Gonzalez Benítez <gonzalezes@uninorte.edu.co>
2022-08-13 19:20:28 -0500

    Added 2 rows of color palettes from Color Hunt

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get_rx/src/rx_typedefs/rx_typedefs.dart';
 
 import '../../utils/color_utils.dart';
 import '../widgets/color_palette.dart';
@@ -25,10 +26,23 @@ class ColorSelectionPage extends StatelessWidget {
           ],
         ),
         // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
-        )));
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ColorPalette(
+                baseColor : '002B5B',
+                baseColor1 : '2B4865',
+                baseColor2 : '256D85',
+                baseColor3 : '8FE3CF',
+                base1Color : '293462',
+                base1Color1 : '1CD6CE',
+                base1Color2 : 'FEDB39',
+                base1Color3 : 'D61C4E',
+                callback: showColor,
+              )
+            ],
+          ))
+        );
   }
 
   // está es la función que será llamada con cada click a un ColorPalette
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -5,24 +5,114 @@ class ColorPalette extends StatelessWidget {
   const ColorPalette({
     Key? key,
     // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
+    required this.baseColor,
+    required this.baseColor1,
+    required this.baseColor2,
+    required this.baseColor3,
+    required this.base1Color,
+    required this.base1Color1,
+    required this.base1Color2,
+    required this.base1Color3,
+    required this.callback,
   }) : super(key: key);
 
   // atributos de la clase, por ejemplo
-  //final String baseColor;
+  final String baseColor;
+  final String baseColor1;
+  final String baseColor2;
+  final String baseColor3;
+  final String base1Color;
+  final String base1Color1;
+  final String base1Color2;
+  final String base1Color3;
 
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback;
 
   @override
   Widget build(BuildContext context) {
     // padding para crear espacio alrededor del widget
-    return Padding(
+    return Container(
+      margin: const EdgeInsets.all(8.0),
       padding: const EdgeInsets.all(8.0),
       // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(baseColor)
+                  ),)
+                ),
+                Expanded(
+                flex: 3,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(baseColor1)
+                  ),)
+                ),
+                Expanded(
+                flex: 2,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(baseColor2)
+                  ),)
+                ),
+                Expanded(
+                flex: 1,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(baseColor3)
+                  ),)
+                )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                flex: 4,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(base1Color)
+                  ),)
+                ),
+                Expanded(
+                flex: 3,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(base1Color1)
+                  ),)
+                ),
+                Expanded(
+                flex: 2,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(base1Color2)
+                  ),)
+                ),
+                Expanded(
+                flex: 1,
+                child: GestureDetector(
+                  child: Container(
+                    height: 100,
+                    color: ColorUtils.FromHex(base1Color3)
+                  ),)
+                )
+            ],
+          )
+        ],
+      )
     );
   }
 }
